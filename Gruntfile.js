85fe2a1d6f66142126a02466a8c7c2c7facba4ce
Jade Rigby <contact@jaderigby.com>
2021-01-23 12:52:32 -0700

    adding data feature

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,14 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		pageContent: grunt.file.readJSON('__data__/content.json'),
 
 		pug: {
 			compile: {
 				options: {
 					pretty: true,
 					data: {
+						pageData: '<%= pageContent %>',
 						debug: false
 					}
 				},
